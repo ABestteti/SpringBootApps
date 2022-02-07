909566210e346e65b0dfbf20e8e761ff1afa9512
Anderson Bestteti <anderson.bestteti@gmail.com>
2022-02-07 19:25:14 -0300

    Added Spring Boot application

---
+++ park-control/src/main/java/com/api/parkcontrol/ParkControlApplication.java
@@ -0,0 +1,13 @@
+package com.api.parkcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkControlApplication.class, args);
+	}
+
+}
