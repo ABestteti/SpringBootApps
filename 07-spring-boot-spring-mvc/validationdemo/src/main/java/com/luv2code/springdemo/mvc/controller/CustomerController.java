09b113298975aa507df2905f4496f570c5c7c41c
Anderson Bestteti <anderson.bestteti@gmail.com>
2024-02-12 10:40:43 -0400

    New application: SpringMVC Validation for Str and Int

--- 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/controller/CustomerController.java
+++ 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/controller/CustomerController.java
@@ -1,12 +1,32 @@
 package com.luv2code.springdemo.mvc.controller;
 
 import com.luv2code.springdemo.mvc.model.Customer;
+import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    // add an initbinder ... to convert trim inpu strings
+    //rempve leading and trailing whitespace
+    // resolve issue for our validation
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+
+        // Pre-process every string form data
+        // Remove leading and trailing whitespace
+        // If string only has white space ... trim it to null.
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model theModel) {
@@ -14,4 +34,20 @@ public class CustomerController {
 
         return "customer-form";
     }
+
+    @PostMapping("processForm")
+    public String processForm(
+            @Valid @ModelAttribute("customer") Customer theCustomer,
+            BindingResult theBindingResult ) {
+
+    System.out.println("Last name: [" + theCustomer.getLastName() + "]");
+        if (theBindingResult.hasErrors()) {
+            return "customer-form";
+        }
+        else {
+            return "customer-confirmation";
+        }
+    }
+
+
 }
--- 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/model/Customer.java
+++ 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/model/Customer.java
@@ -1,14 +1,28 @@
 package com.luv2code.springdemo.mvc.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class Customer {
     private String firstName;
 
-    @NotNull(message = "is required")
-    @Size(min=1, message = "is required")
+    @NotNull(message = "Last name is required")
+    @Size(min=1, message = "Las name is required")
     private String lastName;
+
+    @Min(value = 0, message = "must ve greater than or equal to zero.")
+    @Max(value = 10, message = "must ve less than or equal to 10.")
+    private int freePasses;
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
 
     public String getFirstName() {
         return firstName;
