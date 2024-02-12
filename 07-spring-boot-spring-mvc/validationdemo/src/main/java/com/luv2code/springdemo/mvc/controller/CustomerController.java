38f34072327ab71607ba7ad00e6c41c29650891e
Anderson Bestteti <anderson.bestteti@gmail.com>
2024-02-12 11:17:44 -0400

    New application: SpringMVC Validation custom error msg

--- 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/controller/CustomerController.java
+++ 07-spring-boot-spring-mvc/validationdemo/src/main/java/com/luv2code/springdemo/mvc/controller/CustomerController.java
@@ -15,39 +15,47 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class CustomerController {
 
-    // add an initbinder ... to convert trim inpu strings
-    //rempve leading and trailing whitespace
-    // resolve issue for our validation
-    @InitBinder
-    public void initBinder(WebDataBinder dataBinder) {
+  // add an initbinder ... to convert trim inpu strings
+  // rempve leading and trailing whitespace
+  // resolve issue for our validation
+  @InitBinder
+  public void initBinder(WebDataBinder dataBinder) {
 
-        // Pre-process every string form data
-        // Remove leading and trailing whitespace
-        // If string only has white space ... trim it to null.
-        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
-        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
-    }
+    // Pre-process every string form data
+    // Remove leading and trailing whitespace
+    // If string only has white space ... trim it to null.
+    StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+    dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+  }
 
-    @GetMapping("/")
-    public String showForm(Model theModel) {
-        theModel.addAttribute("customer", new Customer());
+  @GetMapping("/")
+  public String showForm(Model theModel) {
+    theModel.addAttribute("customer", new Customer());
 
-        return "customer-form";
-    }
+    return "customer-form";
+  }
 
-    @PostMapping("processForm")
-    public String processForm(
-            @Valid @ModelAttribute("customer") Customer theCustomer,
-            BindingResult theBindingResult ) {
+  @PostMapping("processForm")
+  public String processForm(
+      @Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
 
     System.out.println("Last name: [" + theCustomer.getLastName() + "]");
-        if (theBindingResult.hasErrors()) {
-            return "customer-form";
-        }
-        else {
-            return "customer-confirmation";
-        }
+
+    // The printout below will provide more details about the exception from the more specific error
+    // type (at field level),
+    // the the more generic. For example:
+    // Binding results:org.springframework.validation.BeanPropertyBindingResult: 1 errors
+    // Field error in object 'customer' on field 'freePasses': rejected value [newnn]; codes
+    // [typeMismatch.customer.freePasses,typeMismatch.freePasses,typeMismatch.java.lang.Integer,typeMismatch]; arguments [org.springframework.context.support.DefaultMessageSourceResolvable: codes [customer.freePasses,freePasses]; arguments []; default message [freePasses]]; default message [Failed to convert property value of type 'java.lang.String' to required type 'java.lang.Integer' for property 'freePasses'; For input string: "newnn"]
+    // Check the messages,properties file for the typeMismatch.customer.freePasses object, and its
+    // customized error merrsage,
+    System.out.println("Binding results:" + theBindingResult);
+    System.out.println("\n\n\n\n");
+
+    if (theBindingResult.hasErrors()) {
+      return "customer-form";
+    } else {
+      return "customer-confirmation";
     }
-
-
+  }
 }
