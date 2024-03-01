5a412b3dd1f02e789864e75abc9e1cca7217b6d1
Anderson Bestteti <anderson.bestteti@gmail.com>
2024-03-01 13:07:52 -0400

    CRUD Database added quick employee view

---
+++ 07-spring-boot-spring-mvc-crud/07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -0,0 +1,37 @@
+package com.luv2code.springboot.cruddemo.controller;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
+    }
+
+    // add mapping for "/list"
+    @GetMapping("/list")
+    public String listEmployees(Model theModel) {
+
+        // get the employees from DB
+        List<Employee> theEmployeeList = employeeService.findAll();
+
+        // add to the model
+        theModel.addAttribute("employees", theEmployeeList);
+
+        // return the name of the view page to show the employee list added
+        // to the model
+        return "list-employees";
+    }
+}
