9630ae41383bf3d6795a0f67c0d04d0d6c532f0f
Anderson Bestteti <anderson.besttetisantos@cgi.com>
2024-07-08 17:19:12 -0300

    APO - Combining multiple cutpoints.

--- 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/aopdemo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/02-spring-boot-aop-demo-pointcut-declarations/aopdemo/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -10,9 +11,10 @@ public class MyDemoLoggingAspect {
 
     // this is where we add all of our related advices for logging
 
-    // let's start with a @Before advice
+    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+    private void forDoaPackage() {}
 
-    @Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+    @Before("forDoaPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n====>>>> Executing @Before advice on addAccount() ...");
     }
