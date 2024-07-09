16a50d9d5cae7207b00f715aaf6da9728d431ebe
Anderson Bestteti <anderson.besttetisantos@cgi.com>
2024-07-09 14:07:30 -0300

    Create demo app to test Spring Actuator

---
+++ 01-spring-boot-overview/03-actuator-demo/src/test/java/com/luv2code/mylcoolapp/MylcoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.mylcoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylcoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
