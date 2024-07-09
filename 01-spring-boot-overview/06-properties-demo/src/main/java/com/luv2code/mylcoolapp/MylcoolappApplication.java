3db827c92a46fb469eeb04fd9137bc4866b64e59
Anderson Bestteti <anderson.besttetisantos@cgi.com>
2024-07-09 15:07:10 -0300

    Added Spring Core projects

---
+++ 01-spring-boot-overview/06-properties-demo/src/main/java/com/luv2code/mylcoolapp/MylcoolappApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.mylcoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylcoolappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylcoolappApplication.class, args);
+	}
+
+}
